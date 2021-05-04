8aba40ae36215039c48eed1cfe83445d9aa0b487
Olenaa Plkc <polokhach.email>
2021-05-04 15:57:48 +0300

    first commit

---
+++ WebApplication8/Controllers/AdminController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Windows.Shapes;
+using WebApplication8.Models;
+using Path = System.IO.Path;
+
+namespace WebApplication8.Controllers
+{
+    public class AdminController : Controller
+    {
+        HOTELEntities db = new HOTELEntities();
+        // GET: Admin
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(tbl_admin avm)
+        {
+            tbl_admin ad = db.tbl_admin.Where(x => x.ad_username == avm.ad_username && x.ad_password == avm.ad_password).SingleOrDefault();
+            if (ad != null)
+            {
+                Session["ad_id"] = ad.ad_id.ToString();
+                return RedirectToAction("Create");
+            }
+            else
+            {
+                ViewBag.error = "Invaid username or password";
+
+            }
+            return View();
+        }
+        public ActionResult Create()
+        {
+            if (Session["ad_id"] == null)
+            {
+                return RedirectToAction("Login");
+
+            }
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(ctgr cvm, HttpPostedFileBase imgfile)
+        {
+            string path = uploadimgfile(imgfile);
+            if (path.Equals("-1"))
+            {
+                ViewBag.error = "Image could not be uploaded....";
+            }
+            else
+            {
+               ctgr cat = new ctgr();
+                cat.cat_name = cvm.cat_name;
+                cat.cat_image = path;
+                cat.cat_status = 1;
+                cat.cat_fk_ad = Convert.ToInt32(Session["ad_id"].ToString());
+                // db.ctgr.Add(cat);
+                db.ctgrs.Add(cat);
+               // db.ctgr.Add(cat);
+                db.SaveChanges();
+                return RedirectToAction("ViewCategory");
+            }
+            return View();
+        }
+        public string uploadimgfile(HttpPostedFileBase file)
+        {
+            Random r = new Random();
+            string path = "-1";
+            int random = r.Next();
+            if (file != null && file.ContentLength > 0)
+            {
+                string extension = Path.GetExtension(file.FileName);
+                if (extension.ToLower().Equals(".jpg") || extension.ToLower().Equals(".jpeg") || extension.ToLower().Equals(".png"))
+                {
+                    try
+                    {
+
+                        path = Path.Combine(Server.MapPath("~/Content/upload"), random + Path.GetFileName(file.FileName));
+                        file.SaveAs(path);
+                        path = "~/Content/upload/" + random + Path.GetFileName(file.FileName);
+
+                        //    ViewBag.Message = "File uploaded successfully";
+                    }
+                    catch (Exception ex)
+                    {
+                        path = "-1";
+                    }
+                }
+                else
+                {
+                    Response.Write("<script>alert('Only jpg ,jpeg or png formats are acceptable....'); </script>");
+                }
+            }
+
+            else
+            {
+                Response.Write("<script>alert('Please select a file'); </script>");
+                path = "-1";
+            }
+
+
+
+            return path;
+        }
+    }
+}
+
+        
+
+
+
+
+       
